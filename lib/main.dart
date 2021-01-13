fcbee11612a5d84183b0c759803c2eeb233a94da
lorenzo esposito <lorenzo@Air-di-Lorenzo.home>
2021-01-13 18:20:13 +0100

    ultime modifiche versione 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,35 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu,),
+          onPressed: () {
+          },
+          tooltip: 'Menù',
+
+        ),
         title: Text('IEEE UNINA'),
         backgroundColor: Colors.blueAccent[400],
-          actions: <Widget>[
+        actions: <Widget>[
+          IconButton(
 
-      Padding(
-          padding: EdgeInsets.only(right: 20.0),
+            icon: Icon(Icons.settings,),
+            onPressed: () {},
+            tooltip: 'Impostazioni',
 
-          child: GestureDetector(
-            onTap: () {help();},
-            child: Icon(
+          ),
+          IconButton(
+            onPressed: () {
+              help();
+            },
+            icon: Icon(
                 Icons.help_rounded
+
             ),
-        ),),],
+            tooltip: 'Scopri chi siamo',
+          ),
+
+        ],
 
         elevation: 50.0, //IconButton
       ), //AppBar
@@ -32,60 +48,84 @@ void main() {
               mainAxisSize: MainAxisSize.max,
 
               children: <Widget>[
-              Expanded(child:  Container(
+                Expanded(child: Container(
                   width: 100,
                   height: 100,
 
-          //        padding:
-        // EdgeInsets.only(right: 30.0,top:30),
-
-              child:  FlatButton(
-                  onPressed: (){ facebook();},
-                  child:Image.asset("images/facebook.png",width:75,height:75,),
+                  //        padding:
+                  // EdgeInsets.only(right: 30.0,top:30),
+child:Tooltip(
+  message: "Facebook",
+                  child: FlatButton(
+                    onPressed: () {
+                      facebook();
+                      },
+                    child: Image.asset(
+                      "images/facebook.png", width: 75, height: 75,),
+                  ),
                 ),
                 ),
-              ),
-
-                Expanded(child:  Container(
+                ),
+                Expanded(child: Container(
                   width: 100,
                   height: 100,
+child:Tooltip(
+  message: "Instagram",
 
-                 child: FlatButton(
-                    onPressed: (){ instagram();},
-                    child:Image.asset("images/instagram.jpeg",width:70,height:70,),
+                  child: FlatButton(
+                    onPressed: () {
+                      instagram();
+                    },
+                    child: Image.asset(
+                      "images/instagram.jpeg", width: 70, height: 70,),
                   ),
 
                 ),),
-
-            ],
+                ),
+              ],
             ),
-        Expanded(child:   Container(
-          width: 500,
-          height: 500,
-              child: Image.asset('images/logo.png',width: 600,height:550,),//logo IEEE
+            Expanded(child: Container(
+              width: 500,
+              height: 500,
+              child: Image.asset('images/logo.png', width: 600, height: 550,),
+              //logo IEEE
+              padding:
+              EdgeInsets.all(40),
             ),),
 
             Row( //seconda riga delle app
               children: <Widget>[
-                Expanded(child:  Container(
+                Expanded(child: Container(
                   width: 100,
                   height: 100,
-                  child:FlatButton(
-                    onPressed: (){ web();},
-                    child:Image.asset("images/web.png",width:80,height:80,),
+                  child:Tooltip(
+                    message: "Website",
+
+                  child: FlatButton(
+                    onPressed: () {
+                      web();
+                    },
+                    child: Image.asset(
+                      "images/web.png", width: 80, height: 80,),
                   ),
 
+                ),
                 ),
                 ),
 
                 Expanded(child: Container(
                   width: 100,
                   height: 100,
-                 child: FlatButton(
-                    onPressed: (){ event();},
-                    child:Image.asset("images/event.png",width:80,height:80,),
+                  child: Tooltip(
+                    message: "Eventbrite",
+                  child: FlatButton(
+                    onPressed: () {
+                      event();
+                    },
+                    child: Image.asset(
+                      "images/event.png", width: 80, height: 80,),
                   ),
-
+                ),
                 ),
                 ),
 
@@ -95,10 +135,12 @@ void main() {
           ],
 
 
+        ),
       ),
-    ),
-    //Scaffold
+      //Scaffold
     ),
     debugShowCheckedModeBanner: false, //Removing Debug Banner
   )); //MaterialApp
 }
+
+
